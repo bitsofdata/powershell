e72ec7a395ecf39749ca31e4b11c56356d007d53
Gustavo Delgado <gaboware@gmail.com>
2022-09-16 08:00:24 -0400

    Update onlineres.ps1

--- onlineres.ps1
+++ onlineres.ps1
@@ -1,1 +1,10 @@
+#find/replace text in a filename
+
 ls *.pdf | Rename-Item -NewName {$_.name -replace "Fiscal2022-2023","EOY2022-2023"}
+
+Get-ChildItem *.pdf | Rename-Item -NewName { $_.Name -replace ' ','' }
+
+#recurses folder and finds all *.pdfs and copies them to another folder
+
+dir -Path "H:\Home Docs" -Filter *.pdf -Recurse | ForEach-Object { Copy-Item $_.FullName "E:\New Home Docs" }
+
