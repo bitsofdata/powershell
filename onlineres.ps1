24839fd8f47c2e0c6710009ffde03034851a77af
Gustavo Delgado <gaboware@gmail.com>
2022-09-16 08:18:44 -0400

    Update onlineres.ps1

--- onlineres.ps1
+++ onlineres.ps1
@@ -10,3 +10,10 @@ dir -Path "H:\Home Docs" -Filter *.pdf -Recurse | ForEach-Object { Copy-Item $_.
 
 #gets file name information from txt file, loops through each file name and copies it to a directory
 get-content diff.txt | foreach-object { copy-item -Path $_ -Destination "c:\files"}
+
+
+#exports folder file information in to excel csv file
+dir | export-csv manifest09092022.csv 
+
+#removes X characters at the end of file and keeps extension
+dir *.pdf | rename-item -newname {$_.name.substring(0, $_.BaseName.length - 13) + $_.Extension}
